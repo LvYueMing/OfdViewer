8a96a9340e3f73b3fbcdef502728151f8db92eae
XunDao <32958845+LvYueMing@users.noreply.github.com>
2026-01-03 22:37:02 +0800

    签名文档

---
+++ OfdViewer/Models/Signature/CheckMethodEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFDViewer.Models.Signature
+{
+    /// <summary>
+    /// 摘要方法
+    /// </summary>
+    public enum CheckMethodEnum
+    {
+        MD5,
+        SHA1
+    }
+}
---
+++ OfdViewer/Models/Signature/Provider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using OFDViewer.Utils;
+
+namespace OFDViewer.Models.Signature
+{
+    /// <summary>
+    /// 创建签名时所用的签章组件提供者信息
+    /// </summary>
+    public class Provider
+    {
+        /// <summary>
+        /// 创建签名时所用的签章组件的提供者名称 
+        /// 必选
+        /// </summary>
+        [XmlAttribute("ProviderName")]
+        [XmlRequired(errorMsg: "ProviderName为必选项，且不能为空")]
+        public string ProviderName { get; set; }
+
+        /// <summary>
+        /// 创建签名时所用的签章组件的版本
+        /// 可选
+        /// </summary>
+        [XmlAttribute("Version")]
+        public string Version { get; set; }
+
+        /// <summary>
+        /// 创建签名时所用的签章组件的制造商 
+        /// 可选
+        /// </summary>
+        [XmlAttribute("Company")]
+        public string Company { get; set; }
+    }
+}
